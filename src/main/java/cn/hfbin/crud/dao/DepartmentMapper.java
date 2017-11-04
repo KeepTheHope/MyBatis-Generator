be8e01aeda62f15fb21bc62aaab6f53bca8e54c5
HuangFuBin <1716850375@qq.com>
2017-11-04 12:53:50 +0800

    MyBatis-Benerator

---
+++ src/main/java/cn/hfbin/crud/dao/DepartmentMapper.java
@@ -0,0 +1,30 @@
+package cn.hfbin.crud.dao;
+
+import cn.hfbin.crud.beans.Department;
+import cn.hfbin.crud.beans.DepartmentExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface DepartmentMapper {
+    int countByExample(DepartmentExample example);
+
+    int deleteByExample(DepartmentExample example);
+
+    int deleteByPrimaryKey(Integer deptId);
+
+    int insert(Department record);
+
+    int insertSelective(Department record);
+
+    List<Department> selectByExample(DepartmentExample example);
+
+    Department selectByPrimaryKey(Integer deptId);
+
+    int updateByExampleSelective(@Param("record") Department record, @Param("example") DepartmentExample example);
+
+    int updateByExample(@Param("record") Department record, @Param("example") DepartmentExample example);
+
+    int updateByPrimaryKeySelective(Department record);
+
+    int updateByPrimaryKey(Department record);
+}
